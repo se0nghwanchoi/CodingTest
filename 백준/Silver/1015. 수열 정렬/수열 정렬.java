753775922d1a35206c4228edcf4e249550d634b8
choisunghw <118499282+choisunghw@users.noreply.github.com>
2023-10-05 17:27:12 +0900

    [Silver IV] Title: 수열 정렬, Time: 148 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = A[i];
+        }
+
+        Arrays.sort(B);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if (A[i] == B[j]){
+                    sb.append(j+ " ");
+                    B[j] = 0;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
